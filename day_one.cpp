d0cd2e1043916355cb87aca8cf7c56726d956693
dayshah <dhyeyhshah@gmail.com>
2023-06-25 19:26:08 -0400

    day one part 2 completed

--- day_one.cpp
+++ day_one.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
+#include <algorithm>
+#include <numeric>
 
 int main() {
     int maxElfCals = 0;
     int currentElfCals = 0;
     std::string calcsStr{};
+    std::vector<int> tripleMax(3, 0);
     while (std::getline(std::cin, calcsStr)) {
-        if (calcsStr == "") {
-            maxElfCals = std::max<int>(currentElfCals, maxElfCals);
-            std::cout << maxElfCals << std::endl;
+        if (calcsStr == "answer") {
+            int result = std::accumulate(
+                tripleMax.begin(),
+                tripleMax.end(),
+                0,
+                [](const int& a, const int& b){
+                    return a + b;
+                }
+            );
+            std::cout << result << std::endl;
+        } else if (calcsStr == "") {
+            if (currentElfCals > tripleMax[2]){
+                tripleMax.pop_back();
+                tripleMax.push_back(currentElfCals);
+                std::sort(tripleMax.begin(), tripleMax.end(), std::greater<int>());
+            }
             currentElfCals = 0;
-            continue;
+        } else {
+            currentElfCals += std::stoi(calcsStr);
         }
-        currentElfCals += std::stoi(calcsStr);
     }
 
     return 0;
